a364252932ad47d48bfbfcbe7614e662659cab81
hykhxy <2371293156@qq.com>
2026-08-18 03:28:38 +0800

    upload server/src/routes/userRoutes.js

---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const wrap = require('../utils/wrap');
+const { requireAuth, optionalAuth } = require('../middlewares/auth');
+const {
+  getPublicProfile,
+  updateMe,
+  changePassword,
+  checkin,
+  checkinStatus,
+  myFavorites
+} = require('../controllers/userController');
+
+const router = express.Router();
+
+// 具名路由须在 /:id 之前注册
+router.put('/me', requireAuth, wrap(updateMe));
+router.put('/me/password', requireAuth, wrap(changePassword));
+router.post('/me/checkin', requireAuth, wrap(checkin));
+router.get('/me/checkin', requireAuth, wrap(checkinStatus));
+router.get('/me/favorites', requireAuth, wrap(myFavorites));
+
+router.get('/:id', optionalAuth, wrap(getPublicProfile));
+
+module.exports = router;
