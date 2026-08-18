9b7b44fff85c2a31f74c84f3f3b834a0fbee7b73
hykhxy <2371293156@qq.com>
2026-08-18 21:04:07 +0800

    upload server/src/routes/authRoutes.js

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const wrap = require('../utils/wrap');
-const { register, login, me } = require('../controllers/authController');
+const { sendCode, register, login, me } = require('../controllers/authController');
 const { requireAuth } = require('../middlewares/auth');
 
 const router = express.Router();
 
+router.post('/send-code', wrap(sendCode));   // 注册邮箱验证码（开发模式回显）
 router.post('/register', wrap(register));
 router.post('/login', wrap(login));
 router.get('/me', requireAuth, wrap(me));
