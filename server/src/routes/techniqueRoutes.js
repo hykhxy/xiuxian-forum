b76fd23097918d22a570a30390d60ce36d3d63d3
hykhxy <2371293156@qq.com>
2026-08-18 14:13:15 +0800

    upload server/src/routes/techniqueRoutes.js

--- server/src/routes/techniqueRoutes.js
+++ server/src/routes/techniqueRoutes.js
@@ -6,8 +6,11 @@ const techniqueCtrl = require('../controllers/techniqueController');
 const router = express.Router();
 
 router.get('/', optionalAuth, wrap(techniqueCtrl.list));
+router.post('/draw', requireAuth, wrap(techniqueCtrl.draw));          // 抽卡（具名路由须在 /:id 前）
+router.get('/backpack', requireAuth, wrap(techniqueCtrl.backpack));   // 功法背包
 router.get('/:id', optionalAuth, wrap(techniqueCtrl.detail));
 router.post('/', requireAuth, wrap(techniqueCtrl.submit));
 router.post('/:id/practice', requireAuth, wrap(techniqueCtrl.practice));
+router.post('/:id/equip', requireAuth, wrap(techniqueCtrl.equip));    // 装备背包功法
 
 module.exports = router;
